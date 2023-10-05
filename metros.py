536a15998aa50f9e99b45a2bcdb4690cdd15da26
Jefferson Melo <pizan666@hotmail.com>
2023-10-05 02:51:26 +0200

    metros

---
+++ metros.py
@@ -0,0 +1,8 @@
+# Solicita ao usuário que insira a medida em metros
+metros = float(input("Por favor, insira a medida em metros: "))
+
+# Converte a medida para centímetros
+centimetros = metros * 100
+
+# Imprime a medida convertida
+print("A medida em centímetros é: ", centimetros)
